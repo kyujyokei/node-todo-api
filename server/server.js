8a87f93759a1f9514c95eebfe04a517ef5314c42
kyujyokei <kyujyokei@gmail.com>
2018-06-29 17:41:27 -0700

    DELETE /todos/:id & test cases completed.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose}  = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -73,6 +75,35 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send({});
     });
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    var body = _.pick(req.body, ['text', 'completed']) // only takes the contents that should be updated
+
+    if (!ObjectID.isValid(id)){
+        res.status(404).send('ID not valid');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+
+    }).catch((err) => {
+        return res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Starting on port ${port}`);
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [{
     text: 'First test todo'
 },{
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 // this funtion will run everytime (every if) before the test case
@@ -162,4 +164,48 @@ describe('DELETE /todos/:id', () => {
             });
     });
 
+});
+
+describe('PATCH /todos/:id', () => {
+
+
+    it('Should update the todo', (done) => {
+
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This is test string from server.test.js';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done)
+    });
+
+    it('Should clear completedAt when completed is false', (done) => {
+
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This is test string from server.test.js';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done)
+    });
 });
